1c3d77769cb964bf4f6ee55d1145021cdf3fc316
Ramitsan <ramitsan@gmail.com>
2022-10-04 01:13:58 +0300

    feat: add donation amount switching on tablet and mobile resolution

--- online-zoo/source/js/script.js
+++ online-zoo/source/js/script.js
@@ -13,3 +13,14 @@
 //     pageHeader.classList.remove('page-header--opened');
 //   }
 // });
+
+// изменение выделенной суммы при загрузке страницы на планшетном и мобильном разрешении
+const donateFormItemOneThousandElement = document.querySelector('#radio-1000');
+const donateFormItemOneHundredElement = document.querySelector('#radio-100');
+
+const mediaQueryTablet = window.matchMedia('(max-width: 999px)')
+// Создаем медиа условие, проверяющее viewports на ширину не более 999 пикселей.
+if (mediaQueryTablet.matches && (donateFormItemOneThousandElement.style.display = 'none')) {
+        donateFormItemOneThousandElement.checked = false;
+        donateFormItemOneHundredElement.checked = true;
+    };
